1b25a39cb9c01237037decde22f48f3f14b1ead1
JeanJacquesNS <jeanjackns7@gmail.com>
2022-03-22 15:37:27 +0200

    Melhorado o codigo de que faz o calculo de horas

--- TimeCalculator.py
+++ TimeCalculator.py
@@ -1,10 +1,44 @@
-def add_time(param1,param2,param3='Monday'):
+param1='11:45 PM'
+param2='1:00'
+param3='Monday'
 	
-	timeframe=param1.split(' ')
-	timeFrame2=timeframe.split(':')
-	result=0
-	
-	if timeframe[1]=='AM':
-		minutes= int(timeFrame2[1])+ int((param2.split(':'))[1])
-		print(minutes)
+timeframe=param1.split(' ')
+
+for time in timeframe:
+	timeFrame2=time.split(':')
+	break
+result=0
+pad='0'
+
+print(timeframe[1],'e issso')
+
+if str(timeframe[1]) == "PM":
+	minutes= int(timeFrame2[1])+ int((param2.split(':'))[1])
+	if minutes>=60:
+		parteInteira= minutes/60
+		resto=minutes%60
+		if resto!=0:
+			horas = int(timeFrame2[0])+int((param2.split(':'))[0])+int(parteInteira)
+			if(horas<12):
+				print(str(horas).rjust(2,pad)+":"+str(resto).rjust(2,pad)+" PM")
+			elif horas>=12:
+				horas=int(horas/12)
+				print(str(horas).rjust(2,pad)+":"+str(resto).rjust(2,pad)+" AM")
+		else:
+			horas = int(timeFrame2[0])+int((param2.split(':'))[0])+int(parteInteira)
+			if(horas<12):
+				print(str(horas).rjust(2,pad)+":"+str(resto).rjust(2,pad)+" PM")
+			elif horas>=12:
+				horas=int(horas/12)
+				print(str(horas).rjust(2,pad)+":"+str(resto).rjust(2,pad)+" AM")
+					
 	else:
+		
+		horas = int(timeFrame2[0])+int((param2.split(':'))[0])
+		print(str(horas).rjust(2,pad)+":"+ str(minutes).rjust(2,pad) +" PM")
+	print(minutes)
+else:
+	print('Yeah')
+	minutes= int(timeFrame2[1])+ int((param2.split(':'))[1])
+	print(minutes)
+
