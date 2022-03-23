60597ff6fcd288ca40893bd5fa72adda15c44def
JeanJacquesNS <jeanjackns7@gmail.com>
2022-03-23 14:23:08 +0200

    Foram acrescentadas mais verificacoes

--- TimeCalculator.py
+++ TimeCalculator.py
@@ -33,11 +33,56 @@ if str(timeframe[1]) == "PM":
 				print(str(horas).rjust(2,pad)+":"+str(resto).rjust(2,pad)+" AM")
 					
 	else:
-		
 		horas = int(timeFrame2[0])+int((param2.split(':'))[0])
-		print(str(horas).rjust(2,pad)+":"+ str(minutes).rjust(2,pad) +" PM")
+		if horas>=12:
+			parteInteiraH= int(horas/12)
+			restoH= horas%12
+			horas= parteInteiraH	
+			print(str(restoH).rjust(2,pad)+":"+ str(minutes).rjust(2,pad) +" AM")
+		else:
+			print(str(horas).rjust(2,pad)+":"+ str(minutes).rjust(2,pad) +" AM")
 	print(minutes)
 else:
+	minutes= int(timeFrame2[1])+ int((param2.split(':'))[1])
+	if minutes>=60:
+		parteInteira= minutes/60
+		resto=minutes%60
+		if resto!=0:
+			horas = int(timeFrame2[0])+int((param2.split(':'))[0])+int(parteInteira)
+			if(horas<12):
+				print(str(horas).rjust(2,pad)+":"+str(resto).rjust(2,pad)+" AM")
+			elif horas>=12 and horas<13:
+				horas=int(horas/12)
+				print(str(horas).rjust(2,pad)+":"+str(resto).rjust(2,pad)+" AM")
+			elif horas>=13:
+				horas=int(horas/12)
+				print(str(horas).rjust(2,pad)+":"+str(resto).rjust(2,pad)+" PM")
+				
+		else:
+			horas = int(timeFrame2[0])+int((param2.split(':'))[0])+int(parteInteira)
+			if(horas<12):
+				print(str(horas).rjust(2,pad)+":"+str(resto).rjust(2,pad)+" AM")
+			elif horas>=12 and horas<13:
+				horas=int(horas/12)
+				print(str(horas).rjust(2,pad)+":"+str(resto).rjust(2,pad)+" AM")
+			elif horas>=13:
+				horas=int(horas/12)
+				print(str(horas).rjust(2,pad)+":"+str(resto).rjust(2,pad)+" PM")
+							
+	else:
+		horas = int(timeFrame2[0])+int((param2.split(':'))[0])
+		if horas>=13 and horas < 14:
+			parteInteiraH= int(horas/12)
+			restoH= horas%12
+			horas= parteInteiraH	
+			print(str(horas).rjust(2,pad)+":"+ str(minutes).rjust(2,pad) +" PM")
+		elif horas>=14:
+			parteInteiraH= int(horas/12)
+			restoH= horas%12
+			horas= restoH	
+			print(str(horas).rjust(2,pad)+":"+ str(minutes).rjust(2,pad) +" PM")
+	
+	print(minutes)
 	print('Yeah')
 	minutes= int(timeFrame2[1])+ int((param2.split(':'))[1])
 	print(minutes)
