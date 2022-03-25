9c95f7995dbe27ab5f10bb235740d619a6b7810e
JeanJacquesNS <jeanjackns7@gmail.com>
2022-03-25 15:08:50 +0200

    Codigo do calculo da area de um poligono concluido

--- PolygonAreaCalculator.py
+++ PolygonAreaCalculator.py
@@ -34,6 +34,7 @@ class Rectangle:
 
     def __str__(self):
         return "Rectangle(width={}, height={})".format(self.width, self.height)
+    
 
 
 class Square(Rectangle):
@@ -53,3 +54,17 @@ class Square(Rectangle):
     def __str__(self):
         return "Square(side={})".format(self.width)
 
+rect = Rectangle(5, 10)
+print(rect.get_area())
+rect.set_width(3)
+print(rect.get_perimeter())
+print(rect)
+print(rect.get_picture())
+
+	
+sq = Square(9)
+print(sq.get_area())
+sq.set_side(4)
+print(sq.get_diagonal())
+print(sq)
+print(sq.get_picture())
