f93b802b7d97e0a3d4bb64a7fcbb94c9f0f77eab
JeanJacquesNS <jeanjackns7@gmail.com>
2022-03-25 13:30:51 +0200

    Objectos adicionados para se poder visualizar o que e feito

--- BudgetApp.py
+++ BudgetApp.py
@@ -88,3 +88,23 @@ def create_spend_chart(categories):
         ss.append(s + " ")
 
     return "\n".join(ss)
+
+food = Category("Food")
+food.deposit(1000, "initial deposit")
+food.withdraw(10.15, "groceries")
+food.withdraw(15.89, "restaurant and more food for dessert")
+
+clothing = Category("Clothing")
+food.transfer(50, clothing)
+clothing.withdraw(25.55)
+clothing.withdraw(100)
+auto = Category("Auto")
+auto.deposit(1000, "initial deposit")
+auto.withdraw(15)
+
+print()
+print(food)
+print()
+print(clothing)
+print()
+print(create_spend_chart([food, clothing, auto]))
